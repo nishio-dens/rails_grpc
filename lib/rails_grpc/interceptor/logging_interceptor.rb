f55e74cb5a1945124764eff9b4d20a34f1fac042
Shinsuke Nishio <nishio@densan-labs.net>
2018-08-23 13:48:07 +0900

    change instrument log level

--- lib/rails_grpc/interceptor/logging_interceptor.rb
+++ lib/rails_grpc/interceptor/logging_interceptor.rb
@@ -10,14 +10,14 @@ module RailsGrpc
       def request_response(request: nil, call: nil, method: nil)
         grpc_method = "#{method.owner.name}##{method.name}"
         logger = @logger
-        logger.debug("[GRPC] #{grpc_method}")
+        logger.info("[GRPC] #{grpc_method}")
         t = Time.now
 
         yield
 
         t = Time.now - t
         display_duration = "%.1f ms" % (t * 1000.0)
-        logger.debug("[GRPC response] (#{display_duration}) #{grpc_method}")
+        logger.info("[GRPC response] (#{display_duration}) #{grpc_method}")
       end
     end
   end
